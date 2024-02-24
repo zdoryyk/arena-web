a958d2189a61b8ad7716e88166a79d3cc9cdafbf
maksymopanasenko <maxpanas008@gmail.com>
2024-02-24 23:52:22 +0100

    add first chart

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -2,18 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { ChartModule } from 'primeng/chart';
 import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
+import { plugins } from 'chart.js';
+
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [ChartModule,AvatarModule,AvatarGroupModule],
+  imports: [AvatarModule, AvatarGroupModule, ChartModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
-export class ProfileComponent {
-
+export class ProfileComponent implements OnInit {
   basicData: any;
 
   basicOptions: any;
 
-  
+  ngOnInit(): void {
+    this.basicData = {
+      labels: ['1', '2', '3', '4', '5', '6'],
+      datasets: [
+        {
+          label: 'Problemset last',
+          data: [10, 40, 90, 50, 100, 120],
+          backgroundColor: '#6e37ed',
+        },
+        {
+          label: 'Problemset first',
+          data: [50, 30, 40, 85, 110, 90],
+          backgroundColor: '#ed6868',
+        },
+      ],
+    };
+    this.basicOptions = {
+      scales: {
+        y: {
+          ticks: {
+            callback: function () {
+              return '';
+            },
+          },
+        },
+        x: {
+          ticks: {
+            callback: function () {
+              return '';
+            },
+          },
+        },
+      },
+    };
+  }
 }
